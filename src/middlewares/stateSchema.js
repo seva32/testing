f8faf30f61881d688c2ed5db7d7ab5170c3b82e4
seva32 <seva32@yahoo.es>
2020-04-29 17:00:05 +0100

    final

---
+++ src/middlewares/stateSchema.js
@@ -0,0 +1,37 @@
+export default {
+  $schema: 'http://json-schema.org/draft-07/schema',
+  $id: 'http://example.com/example.json',
+  type: 'object',
+  title: 'The Root Schema',
+  description: 'The root schema comprises the entire JSON document.',
+  default: {},
+  additionalProperties: true,
+  required: ['comments', 'auth'],
+  properties: {
+    comments: {
+      $id: '#/properties/comments',
+      type: 'array',
+      title: 'The Comments Schema',
+      description: 'An explanation about the purpose of this instance.',
+      default: [],
+      examples: [['seb1', 'seb2']],
+      additionalItems: true,
+      items: {
+        $id: '#/properties/comments/items',
+        type: 'string',
+        title: 'The Items Schema',
+        description: 'An explanation about the purpose of this instance.',
+        default: '',
+        examples: ['seb1', 'seb2'],
+      },
+    },
+    auth: {
+      $id: '#/properties/auth',
+      type: 'boolean',
+      title: 'The Auth Schema',
+      description: 'An explanation about the purpose of this instance.',
+      default: false,
+      examples: [true],
+    },
+  },
+};
